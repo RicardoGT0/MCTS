94098029862efa31590686a28053050afbc7a446
pilachu007 <pikachu007_25@hotmail.com>
2021-02-27 12:15:10 -0600

    corrección archivo

--- calc_score.py
+++ calc_score.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from matplotlib.pyplot import scatter, savefig, figure, plot, clf
 
-dataframe = pd.read_csv('resultados 1/Resultados_100 nodos.csv', index_col=0)
+dataframe = pd.read_csv('resultados/Resultados_100 nodos.csv', index_col=0)
 
 list_func = []
 for index, row in dataframe.iterrows():
@@ -9,17 +9,18 @@ for index, row in dataframe.iterrows():
         list_func.append(row.Funcion)
         
 print (list_func)
-qa
+
 for func in list_func:
     df_slice = dataframe [dataframe ['Funcion'] == func]
     df_slice = df_slice[['Mejor_Valor','Tiempo']]
     print("\n",func)
     print (df_slice.describe())
     func = func[1:-1]
-    df_slice.describe().to_csv('resultados_'+func+'.csv')
-    
-    plot(df_slice['Mejor_Valor']]
-    savefig (''
+    df_slice.describe().to_csv('analisis/resultados_'+func+'.csv')
+   
+    plot(range(1,21), df_slice['Mejor_Valor'])
+    savefig ("analisis/"+func+".png", format="png")
+    clf()
 
 #print (dataframe['Funcion'])
 
