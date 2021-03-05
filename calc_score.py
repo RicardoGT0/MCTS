eb4f6936ab676a5559788787bca67f365a2059fe
pilachu007 <pikachu007_25@hotmail.com>
2021-03-05 09:36:22 -0600

    se agregaron std y mean a la grafica

--- calc_score.py
+++ calc_score.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from matplotlib.pyplot import scatter, savefig, figure, plot, clf
+from matplotlib.pyplot import scatter, savefig, figure, plot, clf, show, legend
+from os import system
 
 dataframe = pd.read_csv('resultados/Resultados_100 nodos.csv', index_col=0)
 
@@ -16,9 +17,18 @@ for func in list_func:
     print("\n",func)
     print (df_slice.describe())
     func = func[1:-1]
-    df_slice.describe().to_csv('analisis/resultados_'+func+'.csv')
-   
+    df_desc = df_slice.describe()
+    df_desc.to_csv('analisis/resultados_'+func+'.csv')
+    std = df_desc.at['std', 'Mejor_Valor']
+    mean = df_desc.at['mean', 'Mejor_Valor']
+    stdmas = std + mean
+    stdmenos = mean-std
+    plot(range(0,22), [mean]*22, 'k--', label='media')
+    plot(range(0,22), [stdmas]*22, 'r--')
+    plot(range(0,22), [stdmenos]*22, 'g--')
     plot(range(1,21), df_slice['Mejor_Valor'])
+    leyendas = ('Media', 'Media + Desviación Estándar', 'Media - Desviación Estándar', 'Mejor valor por ronda')
+    legend(leyendas, loc='upper center', shadow=True, )
     savefig ("analisis/"+func+".png", format="png")
     clf()
 
