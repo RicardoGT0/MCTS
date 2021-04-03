ffc9a765b5bdecf28b371b42cef94398bef30fc0
pikac <pikachu007_25@hotmail.com>
2021-04-03 01:30:23 -0600

    se implementan funciones de prueba CEC2013

--- Main.py
+++ Main.py
@@ -23,6 +23,7 @@ from funciones.Scaffer import Scaffer
 from funciones.Schwefels import Schwefels
 from funciones.Weierstrass import Weierstrass
 from funciones.Xcuadrada import Xcuadrada
+from cec2013lsgo.cec2013 import Benchmark
 
 
 def crear_inicio(tam_arreglo, rango):
@@ -44,71 +45,84 @@ def listToString(s):
         # return string
     return str1
 
-def run(rango = [-100, 100], num_dimension = 10, num_nodos = 100, num_ramas=5, presicion_sim=100):
 
-    funciones=[Bent,
-            Discus,
-            Weierstrass,
-            Schwefels,
-            Katsuura,
-            HappyCat,
-            HGBat,
-            Expanded_GR,
-            Scaffer,
-            Rosenbrock,
-            Griewank,
-            Rastrigin,
-            Elliptic,
-            Ackley,
-            Xcuadrada]
-
+def run(rango=[-100, 100], num_dimension=10, num_nodos=100, num_ramas=5, presicion_sim=100, rondas=20, funcion=Xcuadrada, directorio=""):
     data = []
-    directorio= "resultados/resultados_D"+str(num_dimension)+"_N"+str(num_nodos)+"_R"+str(num_ramas)+"_P"+str(presicion_sim)
-    try:
-        mkdir(directorio)
-        f = open('last_path.txt', 'w')
-        f.write(directorio)
-        f.close()
-    except:
-        print("El directorio ", directorio, " ya existe")
-    for funcion in funciones:
-        Nodo = Dyn_Nodo.dyn_nodo(funcion)
-        print(funcion)
+    Nodo = Dyn_Nodo.dyn_nodo(funcion)
+    print(funcion)
 
-        for ronda in range(1):
-            t_inicial = time()
+    for ronda in range(rondas):
+        t_inicial = time()
 
-            inicio = crear_inicio(num_dimension, rango)
-            MCTS=MCTS_UCT(Nodo, inicio,num_nodos,num_ramas, rango, presicion_sim, directorio)
+        inicio = crear_inicio(num_dimension, rango)
+        MCTS = MCTS_UCT(Nodo, inicio, num_nodos, num_ramas, rango, presicion_sim, directorio)
 
-            tiempo = time() - t_inicial
-            valor_inicial=MCTS.nodo_inicial.getValor()
-            vector_inicial=MCTS.nodo_inicial.getInformacion()
-            mejor_valor=MCTS.Mejor_valor
-            mejor_vector=MCTS.Mejor_vector
-            data.append([funcion, ronda, vector_inicial, valor_inicial, mejor_valor, mejor_vector, tiempo])
+        tiempo = time() - t_inicial
+        valor_inicial = MCTS.nodo_inicial.getValor()
+        vector_inicial = MCTS.nodo_inicial.getInformacion()
+        mejor_valor = MCTS.Mejor_valor
+        mejor_vector = MCTS.Mejor_vector
+        data.append([funcion, ronda, vector_inicial, valor_inicial, mejor_valor, mejor_vector, tiempo])
 
-
-            x = range(1, num_nodos+1)
-            plot(x,MCTS.hist_valor)
-            n_funcion=str(funcion)[1:-1]
-            n_fig = listToString([directorio+"/Figura_",n_funcion, "_", str(ronda),".png"])
-            savefig(n_fig, format="png")
-            clf()
-
+        x = range(1, num_nodos + 1)
+        plot(x, MCTS.hist_valor)
+        n_funcion = str(funcion)[1:-1]
+        n_fig = listToString([directorio + "/Figura_", n_funcion, "_", str(ronda), ".png"])
+        savefig(n_fig, format="png")
+        clf()
 
     dataframe = pd.DataFrame(data, columns=["Funcion", "Ronda", "Vector_Inicial", "Valor_Inicial", "Mejor_Valor", "Mejor_Vector", "Tiempo"])
-    dataframe.to_csv(directorio+"/Resultados.csv")
+    dataframe.to_csv(directorio + "/Resultados.csv")
 
-    calc_score()
-    comparativa()
+
 
 
 if __name__ == "__main__":
-    run(num_dimension=2, num_nodos=60, num_ramas=3, presicion_sim=10)
+    bench=Benchmark()
+    try:
+        mkdir("resultados")
+    except:
+        print("Error al crear el directorio /resultados")
+
+    for nd in [300]:
+        for nr in [7]:
+            for p in [200, 300]:
+                directorio = "resultados/resultados_N" + str(nd) + "_R" + str(nr) + "_P" + str(p)
+                try:
+                    mkdir(directorio)
+                    f = open('last_path.txt', 'w')
+                    f.write(directorio)
+                    f.close()
+                except:
+                    print("Error al crear el directorio ", directorio)
+
+                for f in range(1, 16):
+                    info = bench.get_info(f)
+                    dim = info['dimension']
+                    low = info['lower']
+                    upp = info['upper']
+                    func_fit = bench.get_function(f)
+                    func=type("func_" + str(f),(object,),{"func":func_fit})
+                    run(rango=[low,upp], num_dimension=dim, funcion=func,rondas=20, num_nodos= nd, num_ramas=nr, presicion_sim=p, directorio=directorio)
+
+                calc_score()
+                comparativa(directorio)
 """
-    for nd in range(50,501,50):
-        for nr in range(2,11):
-            for p in range(50, 301, 50):
-                run(num_nodos=nd,num_ramas=nr,presicion_sim=p)
+    funciones = [Bent,
+                 Discus,
+                 Weierstrass,
+                 Schwefels,
+                 Katsuura,
+                 HappyCat,
+                 HGBat,
+                 Expanded_GR,
+                 Scaffer,
+                 Rosenbrock,
+                 Griewank,
+                 Rastrigin,
+                 Elliptic,
+                 Ackley,
+                 Xcuadrada]
+    
+
 """
